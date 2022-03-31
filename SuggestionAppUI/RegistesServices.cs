cd51137cac837ccdf91a3764b418fe33798741b7
Leandro Reis <leandro.reis@nextbitt.com>
2022-03-31 09:31:03 +0100

    Add ConfigureServices class

---
+++ SuggestionAppUI/RegistesServices.cs
@@ -0,0 +1,10 @@
+﻿namespace SuggestionAppUI;
+
+public static class RegistesServices
+{
+    public static void ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+    }
+}
