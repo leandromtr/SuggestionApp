2ce404971bb1e7e959e824455d3d58044a9e9e64
Leandro Reis <leandro.reis@nextbitt.com>
2022-03-31 20:30:25 +0100

    Add MongoUserData class and IUserData Interface

---
+++ SuggestionAppLibrary/DataAccess/IUserData.cs
@@ -0,0 +1,11 @@
+﻿
+namespace SuggestionAppLibrary.DataAccess;
+
+public interface IUserData
+{
+    Task CreateUser(UserModel user);
+    Task<UserModel> GetUser(string id);
+    Task<List<UserModel>> GetUserAsync();
+    Task<UserModel> GetUserFromAuthentication(string objectId);
+    Task UpdateUser(UserModel user);
+}
