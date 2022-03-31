61f4fffbfe9ed66549dc0ae9e3eae20830be7a59
Leandro Reis <leandro.reis@nextbitt.com>
2022-03-31 10:54:46 +0100

    Add BasicUserModel

---
+++ SuggestionAppLibrary/Models/BasicUserModel .cs
@@ -0,0 +1,18 @@
+﻿namespace SuggestionAppLibrary.Models;
+public class BasicUserModel
+{
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+    public string DisplayName { get; set; }
+
+    public BasicUserModel()
+    {
+            
+    }
+
+    public BasicUserModel(UserModel userModel)
+    {
+        Id = userModel.Id;
+        DisplayName = userModel.DisplayName;    
+    }
+}
