9c1528fb5ca02b5741970984954241cfc8ed5749
Leandro Reis <leandro.reis@nextbitt.com>
2022-04-07 08:42:29 +0100

    Add Profile.razor page

--- SuggestionAppLibrary/DataAccess/ISuggestionData.cs
+++ SuggestionAppLibrary/DataAccess/ISuggestionData.cs
@@ -8,6 +8,7 @@ public interface ISuggestionData
     Task<List<SuggestionModel>> GetAllSuggestionsWaitingForApproval();
     Task<SuggestionModel> GetSuggestion(string id);
     Task<List<SuggestionModel>> GetSuggestions();
+    Task<List<SuggestionModel>> GetUsersSuggestions(string userId);
     Task UpdateSuggestions(SuggestionModel suggestion);
     Task UpvoteSuggestion(string suggestionId, string userId);
 }
